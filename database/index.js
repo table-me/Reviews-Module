131893c7902221548af93dd3a1fc79a2e46c1c1e
Jia <jiafeng3124@gmail.com>
2019-03-27 12:00:46 -0700

    implement schema for user and restaurant

--- database/index.js
+++ database/index.js
@@ -1,3 +1,43 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/reviews', {useNewUrlParser: true});
+const db = mongoose.connect('mongodb://localhost/reviews', {useNewUrlParser: true});
+mongoose.Promise = global.Promise;
 
+const userSchema = new mongoose.Schema({
+  _id: String,
+  name: String,
+  profilePic: String,
+  city: String,
+  totalReviews: Number,
+  reviews: {
+    reviewId: String,
+    restaurantId: Number,
+    review: String,
+    ratings: {
+      overall: Number,
+      food: Number,
+      ambiance: Number,
+      service: Number,
+      value: Number
+    },
+    createdAt: Date
+  }
+});
+
+const restaurantSchema = new mongoose.Schema({
+  _id: String,
+  totalReviews: Number,
+  stars: {
+    five: [String],
+    four: [String],
+    three: [String],
+    two: [String],
+    one: [String]
+  },
+  ratings: {
+    overall: Number,
+    food: Number,
+    ambiance: Number,
+    service: Number,
+    value: Number
+  }
+});
