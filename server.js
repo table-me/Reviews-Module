b37ca3f4888552cc257dc0b406bf36f09405c364
Jia <jiafeng3124@gmail.com>
2019-05-29 14:48:00 -0700

    delete path

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const Restaurant = require("./database/Restaurant.js");
-const path = require("path");
 
 const app = express();
 const port = 3004;
