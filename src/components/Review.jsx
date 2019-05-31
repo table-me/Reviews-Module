b8e8c9fd203900b0de7b1b50102787a3d9d91e16
Jia <jiafeng3124@gmail.com>
2019-05-31 11:51:38 -0700

    formating and fix upvote color bug

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import axios from 'axios';
-import ReportPopUp from './ReportPopUp.jsx';
-import moment from 'moment';
-import styles from './Review.css';
+import React from "react";
+import PropTypes from "prop-types";
+import axios from "axios";
+import ReportPopUp from "./ReportPopUp.jsx";
+import moment from "moment";
+import styles from "./Review.css";
 
 class Review extends React.Component {
   constructor(props) {
@@ -11,13 +11,15 @@ class Review extends React.Component {
     this.state = {
       hoveronHelp: false,
       helpful: false,
-      upvoteIcon: this.props.review.is_helpful ? 'https://s3-us-west-1.amazonaws.com/review-photos-fec-open-table/redUpvote.png' : 'https://s3-us-west-1.amazonaws.com/review-photos-fec-open-table/whiteUpvote.png',
+      upvoteIcon: this.props.review.is_helpful
+        ? "https://s3-us-west-1.amazonaws.com/review-photos-fec-open-table/redUpvote.png"
+        : "https://s3-us-west-1.amazonaws.com/review-photos-fec-open-table/whiteUpvote.png",
       readMoreClicked: false,
       reviewText: this.props.review.review.slice(0, 200),
       stars: [],
       reportClicked: false,
-      reportPopUp: '',
-      randomColor: '#ffffff'
+      reportPopUp: "",
+      randomColor: "#ffffff"
     };
   }
 
@@ -26,8 +28,8 @@ class Review extends React.Component {
     this.setStars();
     this.setColor();
     if (review.is_helpful) this.setState({ helpful: true });
-    if (review.review.length > 200);
-    this.setState({ reviewText: review.review.slice(0, 200) + '...' });
+    if (review.review.length > 300);
+    this.setState({ reviewText: review.review.slice(0, 200) + "..." });
   }
 
   setStars() {
@@ -36,16 +38,22 @@ class Review extends React.Component {
     let initialRating = review.overallRating;
     for (let i = 0; i < 5; i++) {
       initialRating > 0.5
-        ? stars.push("https://s3-us-west-1.amazonaws.com/review-photos-fec-open-table/redStar.png") 
-        : stars.push("https://s3-us-west-1.amazonaws.com/review-photos-fec-open-table/greyStar.png");
+        ? stars.push(
+            "https://s3-us-west-1.amazonaws.com/review-photos-fec-open-table/redStar.png"
+          )
+        : stars.push(
+            "https://s3-us-west-1.amazonaws.com/review-photos-fec-open-table/greyStar.png"
+          );
       initialRating--;
     }
     this.setState({ stars });
   }
 
   setColor() {
-    const circleColors = ['#df4e96', '#bb6acd', '#6c8ae4', '#d86441'];
-    this.setState({ randomColor: circleColors[Math.floor(Math.random() * circleColors.length)] });
+    const circleColors = ["#df4e96", "#bb6acd", "#6c8ae4", "#d86441"];
+    this.setState({
+      randomColor: circleColors[Math.floor(Math.random() * circleColors.length)]
+    });
   }
 
   setNode(node) {
@@ -57,19 +65,27 @@ class Review extends React.Component {
     const { helpful } = this.state;
     this.setState({ helpful: !helpful });
     helpful
-      ? this.setState({ upvoteIcon: 'https://s3-us-west-1.amazonaws.com/review-photos-fec-open-table/redUpvote.png' }) 
-      : this.setState({ upvoteIcon: 'https://s3-us-west-1.amazonaws.com/review-photos-fec-open-table/whiteUpvote.png' });
-    review.is_helpful ? review.is_helpful = 0 : review.is_helpful = 1;
+      ? this.setState({
+          upvoteIcon:
+            "https://s3-us-west-1.amazonaws.com/review-photos-fec-open-table/whiteUpvote.png"
+        })
+      : this.setState({
+          upvoteIcon:
+            "https://s3-us-west-1.amazonaws.com/review-photos-fec-open-table/redUpvote.png"
+        });
+    review.is_helpful ? (review.is_helpful = 0) : (review.is_helpful = 1);
   }
 
   readMoreToggle(e) {
     const { review } = this.props;
     const { reviewText } = this.state;
     e.preventDefault();
-    this.setState(prevState => ({ readMoreClicked: !prevState.readMoreClicked}));
-    (reviewText.length < 305)
-      ? this.setState({ reviewText: review.review }) 
-      : this.setState({ reviewText: review.review.slice(0, 200) + '...' });
+    this.setState(prevState => ({
+      readMoreClicked: !prevState.readMoreClicked
+    }));
+    reviewText.length < 300
+      ? this.setState({ reviewText: review.review })
+      : this.setState({ reviewText: review.review.slice(0, 200) + "..." });
   }
 
   toggleReportModal() {
@@ -84,66 +100,93 @@ class Review extends React.Component {
 
   reportPopUp() {
     const { reportClicked } = this.state;
-    this.setState({ reportPopUp: (reportClicked
-      ? (
-        <ReportPopUp 
-          setNode={this.setNode.bind(this)} 
+    this.setState({
+      reportPopUp: reportClicked ? (
+        <ReportPopUp
+          setNode={this.setNode.bind(this)}
           outsideClick={this.handleOutsideClick.bind(this)}
-          toggleReportModal={this.toggleReportModal.bind(this)}/>
+          toggleReportModal={this.toggleReportModal.bind(this)}
+        />
+      ) : (
+        ""
       )
-      : '')
     });
   }
 
   render() {
     const { review } = this.props;
-    const { hoveronHelp, readMoreClicked, reportPopUp, randomColor, stars, reviewText, upvoteIcon, helpful } = this.state;
-    const helpHover = (hoveronHelp ? 'helpHovered' : 'placeholder');
-    let readMorePhrase = (readMoreClicked ? '- Read less' : '+ Read more');
-    if (!readMoreClicked && review.review.length < 200) readMorePhrase = '';
-    const reviewPluralCase = (review.reviewCount === 1 ? 'review' : 'reviews');
+    const {
+      hoveronHelp,
+      readMoreClicked,
+      reportPopUp,
+      randomColor,
+      stars,
+      reviewText,
+      upvoteIcon,
+      helpful
+    } = this.state;
+    const helpHover = hoveronHelp ? "helpHovered" : "placeholder";
+    let readMorePhrase = readMoreClicked ? "- Read less" : "+ Read more";
+    if (!readMoreClicked && review.review.length < 200) readMorePhrase = "";
+    const reviewPluralCase = review.reviewCount === 1 ? "review" : "reviews";
 
     return (
       <div id="reviewContainer">
         {reportPopUp}
         <div className="twoHalvesContainer">
-
           <div className="leftHalf" id="reviewLeftHalf">
             <div id="reviewCircleContainer">
-              <div className="authorCircle" style={{ backgroundColor: randomColor }}>
+              <div
+                className="authorCircle"
+                style={{ backgroundColor: randomColor }}
+              >
                 <div id="reviewInitials">{review.initials}</div>
               </div>
             </div>
             <div id="usernameContainer">
               <span>
-                <span id="reviewUsername">{review.username}</span>
+                <span id="reviewUsername">{review.username.slice(0, 8)}</span>
               </span>
             </div>
-            <span id="userCity">{review.city}</span>
+            <span id="userCity">{review.city.slice(0, 11)}</span>
             <div id="userReviewsContainer">
               <span className="commentIcon" />
-              <span id="reviewCountText">&nbsp; {review.reviewCount} {reviewPluralCase}</span>
+              <span id="reviewCountText">
+                {review.reviewCount} {reviewPluralCase}
+              </span>
             </div>
           </div>
 
           <div className="rightHalf" id="reviewRightHalf">
-
             <div id="reviewStarsDateRating">
               <div id="reviewStarsDate">
                 <div id="reviewStarsContainer">
-                  {stars.map(star => <img className="reviewStar" src={star} alt="Star Icon" />)}
+                  {stars.map(star => (
+                    <img className="reviewStar" src={star} alt="Star Icon" />
+                  ))}
                 </div>
-                <span className="reviewRatingDate"> Dined on {moment(review.createdAt).format('MMMM Do, YYYY')}</span>
+                <span className="reviewRatingDate">
+                  {" "}
+                  Dined on {moment(review.createdAt).format("MMMM Do, YYYY")}
+                </span>
               </div>
               <div id="reviewRatingsContainer">
                 <span className="reviewRatingCategory">Overall </span>
-                <span className="reviewRatingNumber">{review.overallRating} &nbsp;</span>
+                <span className="reviewRatingNumber">
+                  {review.overallRating} &nbsp;
+                </span>
                 <span className="reviewRatingCategory">&#8226; Food </span>
-                <span className="reviewRatingNumber">{review.foodRating} &nbsp;</span>
+                <span className="reviewRatingNumber">
+                  {review.foodRating} &nbsp;
+                </span>
                 <span className="reviewRatingCategory">&#8226; Service </span>
-                <span className="reviewRatingNumber">{review.serviceRating} &nbsp;</span>
+                <span className="reviewRatingNumber">
+                  {review.serviceRating} &nbsp;
+                </span>
                 <span className="reviewRatingCategory">&#8226; Ambiance </span>
-                <span className="reviewRatingNumber">{review.ambianceRating}</span>
+                <span className="reviewRatingNumber">
+                  {review.ambianceRating}
+                </span>
               </div>
             </div>
 
@@ -153,26 +196,50 @@ class Review extends React.Component {
 
             <div id="reportHelpful">
               <div>
-                <a id="readMore" href="#" onClick={(e) => this.readMoreToggle(e)}>{readMorePhrase}</a>
+                <a id="readMore" href="#" onClick={e => this.readMoreToggle(e)}>
+                  {readMorePhrase}
+                </a>
               </div>
               <div id="subReportHelpful">
-                <div className="flexCenter" id="reportContainer" onClick={(e) => this.toggleReportModal(e)}>
+                <div
+                  className="flexCenter"
+                  id="reportContainer"
+                  onClick={e => this.toggleReportModal(e)}
+                >
                   <div id="flagIcon" />
                   <span className="reportText">Report</span>
                 </div>
-                <div className="flexCenter"
-                  id={helpHover} 
+                <div
+                  className="flexCenter"
+                  id={helpHover}
                   value={review.is_helpful}
-                  onClick={() => this.helpfulClick(review.is_helpful)} 
-                  onMouseOver={() => this.setState({ hoveronHelp: true, upvoteIcon: 'https://s3-us-west-1.amazonaws.com/review-photos-fec-open-table/redUpvote.png' })} 
-                  onMouseLeave={() => {
-                    this.setState({ hoveronHelp: false });
-                    helpful ? this.setState({ upvoteIcon: 'https://s3-us-west-1.amazonaws.com/review-photos-fec-open-table/redUpvote.png' }) : this.setState({ upvoteIcon: 'https://s3-us-west-1.amazonaws.com/review-photos-fec-open-table/whiteUpvote.png' });
-                  }}>
-                  <div className="flex" >
+                  onClick={() => this.helpfulClick(review.is_helpful)}
+                  // onMouseOver={() =>
+                  //   this.setState({
+                  //     hoveronHelp: true,
+                  //     upvoteIcon:
+                  //       "https://s3-us-west-1.amazonaws.com/review-photos-fec-open-table/redUpvote.png"
+                  //   })
+                  // }
+                  // onMouseLeave={() => {
+                  //   this.setState({ hoveronHelp: false });
+                  //   helpful
+                  //     ? this.setState({
+                  //         upvoteIcon:
+                  //           "https://s3-us-west-1.amazonaws.com/review-photos-fec-open-table/redUpvote.png"
+                  //       })
+                  //     : this.setState({
+                  //         upvoteIcon:
+                  //           "https://s3-us-west-1.amazonaws.com/review-photos-fec-open-table/whiteUpvote.png"
+                  //       });
+                  // }}
+                >
+                  <div className="flex">
                     <img id="upvoteIcon" src={upvoteIcon} alt="upvote Icon" />
                   </div>
-                  <span className="reportText">Helpful {helpful ? '(1)' : ''}</span>
+                  <span className="helpfulText">
+                    Helpful {helpful ? "(1)" : ""}
+                  </span>
                 </div>
               </div>
             </div>
@@ -195,6 +262,6 @@ Review.propTypes = {
     noise: PropTypes.number.isRequired,
     review: PropTypes.string.isRequired,
     username: PropTypes.string.isRequired,
-    reviewCount: PropTypes.number.isRequired,
-  }).isRequired,
+    reviewCount: PropTypes.number.isRequired
+  }).isRequired
 };
