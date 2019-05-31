293ed56d74e5a7213a32f91f46d5465fbd7ff967
Jia <jiafeng3124@gmail.com>
2019-05-30 20:55:56 -0700

    add classes

--- src/components/ReviewSummary.jsx
+++ src/components/ReviewSummary.jsx
@@ -1,91 +1,152 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import RatingBar from './RatingBar.jsx';
-import styles from './ReviewSummary.css';
+import React from "react";
+import PropTypes from "prop-types";
+import RatingBar from "./RatingBar.jsx";
+import styles from "./ReviewSummary.css";
 
-const ReviewSummary = ({ ratings, recommended, allReviews, stars, filter, scrollToTopOfFeed, percentages }) => {
-    let noiseLevel;
-    if (ratings.noise > 2) {
-      noiseLevel = 'Loud';
-    } else if (ratings.noise < 2 && ratings.noise > 1) {
-      noiseLevel = 'Moderate';
-    } else {
-      noiseLevel = 'Quiet';
-    }
-    if (!percentages.length) {
-      return <div>Please wait</div>
-    }
-    const starSource = (stars ? stars : Array(5).fill(''));
-    return (
-      <div id="reviewSummaryContainer">
-        <div className="summaryHeader">What {allReviews.length} People Are Saying</div>
-        <div>
-
-          <div id="leftSummaryContainer">
-            <div className="summarySubHeader"><strong>Overall ratings and reviews</strong></div>
-            <div id="reviewConditional">Reviews can only be made by diners who have eaten at this restaurant</div>
-            <div>
-              <div className="summaryStarRating">
-                {stars.map((star, i) => <span><img className="summaryStarIcon" src={starSource[i]} alt="Star Icon" /></span>)}
-              </div>
-              <div className="summaryStarRating" id="summaryStarText">
-                <span> &nbsp; {ratings.totalAverage}</span>
-                <span> Based on Recent Ratings</span>
-              </div>
+const ReviewSummary = ({
+  ratings,
+  recommended,
+  allReviews,
+  stars,
+  filter,
+  scrollToTopOfFeed,
+  percentages
+}) => {
+  let noiseLevel;
+  if (ratings.noise > 2) {
+    noiseLevel = "Loud";
+  } else if (ratings.noise < 2 && ratings.noise > 1) {
+    noiseLevel = "Moderate";
+  } else {
+    noiseLevel = "Quiet";
+  }
+  if (!percentages.length) {
+    return <div>Please wait</div>;
+  }
+  const starSource = stars ? stars : Array(5).fill("");
+  return (
+    <div id="reviewSummaryContainer">
+      <div className="summaryHeader">
+        What {allReviews.length} People Are Saying
+      </div>
+      <div id="summaryContainer">
+        <div id="leftSummaryContainer">
+          <div className="summarySubHeader">
+            <span>Overall ratings and reviews</span>
+          </div>
+          <div id="reviewConditional">
+            Reviews can only be made by diners who <br />
+            have eaten at this restaurant
+          </div>
+          <div>
+            <div className="summaryStarRating">
+              {stars.map((star, i) => (
+                <span>
+                  <img
+                    className="summaryStarIcon"
+                    src={starSource[i]}
+                    alt="Star Icon"
+                  />
+                </span>
+              ))}
             </div>
-            <div>
-              <div className="summaryRating" id="summaryFirstRating">
-                <div><strong>{ratings.foodAverage}</strong></div>
-                <div className="summaryCategory">Food</div>
-              </div>
-              <div className="summaryRating">
-                <div><strong>{ratings.serviceAverage}</strong></div>
-                <div className="summaryCategory">Service</div>
-              </div>
-              <div className="summaryRating">
-                <div><strong>{ratings.ambianceAverage}</strong></div>
-                <div className="summaryCategory">Ambiance</div>
-              </div>
-              <div className="summaryRating">
-                <div><strong>{ratings.valueAverage}</strong></div>
-                <div className="summaryCategory">Value</div>
-              </div>
-            </div>
-            <div className="summarySpacingContainer">
-              <div className="inlineBlock">
-                <span><img className="summaryIcon" id="summaryBarIcon" src="https://s3-us-west-1.amazonaws.com/review-photos-fec-open-table/risingBars.png" alt="bar icon" /></span>
-                <span id="summaryNoiseText"><strong>Noise &#8226;</strong><span id="summaryNoiseLevel"> {noiseLevel}</span></span>
-              </div>
-            </div>
-            <div className="summarySpacingContainer">
-              <div className="inlineBlock">
-                <span><img className="summaryIcon" id="thumbsUpIcon" src="https://s3-us-west-1.amazonaws.com/review-photos-fec-open-table/thumbsUp.png" alt="thumbsUp icon" /></span>
-                <span id="recommendedText"><strong>{recommended}% of people</strong> <span>would recommend it to a friend</span></span>
-              </div>
+            <div className="summaryStarRating" id="summaryStarText">
+              <span> &nbsp; {ratings.totalAverage} &nbsp;</span>
+              <span> based on recent ratings</span>
             </div>
           </div>
-
-          <div id="summaryToolbarContainer">
-            <div>
-              {percentages.map((bar, index) => 
-                (<div className="toolbarAndNumber" onClick={() => {filter(5 - index); scrollToTopOfFeed();}}>
-                  <span className="toolbarNumber">{5 - index}</span>
-                  <div className="toolbar-light-background">
-                    <div className="toolbar-red" style={{ width: percentages[4 - index] }} key={percentages[4 - index]} />
-                  </div>
-                </div>)
-              )}
+          <div className="summaryRatingContainer">
+            <div className="summaryRating" id="summaryFirstRating">
+              <div>
+                <strong>{ratings.foodAverage}</strong>
+              </div>
+              <div className="summaryCategory">Food</div>
+            </div>
+            <div className="summaryRating">
+              <div>
+                <strong>{ratings.serviceAverage}</strong>
+              </div>
+              <div className="summaryCategory">Service</div>
+            </div>
+            <div className="summaryRating">
+              <div>
+                <strong>{ratings.ambianceAverage}</strong>
+              </div>
+              <div className="summaryCategory">Ambience</div>
+            </div>
+            <div className="summaryRating">
+              <div>
+                <strong>{ratings.valueAverage}</strong>
+              </div>
+              <div className="summaryCategory">Value</div>
+            </div>
+          </div>
+          <div className="summarySpacingContainer">
+            <div className="inlineBlock">
+              <span>
+                <img
+                  className="summaryIcon"
+                  id="summaryBarIcon"
+                  src="https://s3-us-west-1.amazonaws.com/review-photos-fec-open-table/risingBars.png"
+                  alt="bar icon"
+                />
+              </span>
+              <span id="summaryNoiseText">
+                <strong>Noise &#8226;</strong>
+                <span id="summaryNoiseLevel"> {noiseLevel}</span>
+              </span>
+            </div>
+          </div>
+          <div className="summarySpacingContainer">
+            <div className="inlineBlock">
+              <span>
+                <img
+                  className="summaryIcon"
+                  id="thumbsUpIcon"
+                  src="https://s3-us-west-1.amazonaws.com/review-photos-fec-open-table/thumbsUp.png"
+                  alt="thumbsUp icon"
+                />
+              </span>
+              <span id="recommendedText">
+                <strong>{recommended}% of people</strong>{" "}
+                <span>would recommend it to a friend</span>
+              </span>
             </div>
           </div>
         </div>
 
-        <div>
-          <a id="BestRestaurantsLink" href="#">Best Restaurants in San Francisco</a>
+        <div id="summaryToolbarContainer">
+          <div id="toolbar">
+            {percentages.map((bar, index) => (
+              <div
+                className="toolbarAndNumber"
+                onClick={() => {
+                  filter(5 - index);
+                  scrollToTopOfFeed();
+                }}
+              >
+                <span className="toolbarNumber">{5 - index}</span>
+                <div className="toolbar-light-background">
+                  <div
+                    className="toolbar-red"
+                    style={{ width: percentages[4 - index] }}
+                    key={percentages[4 - index]}
+                  />
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
-    );
-  }
 
+      <div>
+        <a id="BestRestaurantsLink" href="#">
+          Best Restaurants in San Francisco
+        </a>
+      </div>
+    </div>
+  );
+};
 
 export default ReviewSummary;
 
--- src/components/ReviewToolbar.jsx
+++ src/components/ReviewToolbar.jsx
@@ -1,17 +1,18 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import FilterBox from './FilterBox.jsx';
-import styles from './ReviewToolbar.css';
-import UnCheckedIcon from './UnCheckedIcon.jsx';
+import React from "react";
+import PropTypes from "prop-types";
+import FilterBox from "./FilterBox.jsx";
+import styles from "./ReviewToolbar.css";
+import UnCheckedIcon from "./UnCheckedIcon.jsx";
 
 class ReviewToolbar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       ddSelected: false,
-      display: 'none',
-      current: 'Newest',
-      arrow: 'https://s3-us-west-1.amazonaws.com/review-photos-fec-open-table/downArrow.png'
+      display: "none",
+      current: "Newest",
+      arrow:
+        "https://s3-us-west-1.amazonaws.com/review-photos-fec-open-table/downArrow.png"
     };
   }
 
@@ -19,13 +20,21 @@ class ReviewToolbar extends React.Component {
     const { ddSelected } = this.state;
     const { scrollToTopOfFeed } = this.props;
     this.setState({ ddSelected: !ddSelected });
-    e.target.textContent ? this.setState({ current: e.target.textContent }) : null;
+    e.target.textContent
+      ? this.setState({ current: e.target.textContent })
+      : null;
     if (!ddSelected) {
-      this.setState({ display: 'block' });
-      this.setState({ arrow: 'https://s3-us-west-1.amazonaws.com/review-photos-fec-open-table/upArrow.png' });
+      this.setState({ display: "block" });
+      this.setState({
+        arrow:
+          "https://s3-us-west-1.amazonaws.com/review-photos-fec-open-table/upArrow.png"
+      });
     } else {
-      this.setState({ display: 'none' });
-      this.setState({ arrow: 'https://s3-us-west-1.amazonaws.com/review-photos-fec-open-table/downArrow.png' });
+      this.setState({ display: "none" });
+      this.setState({
+        arrow:
+          "https://s3-us-west-1.amazonaws.com/review-photos-fec-open-table/downArrow.png"
+      });
       scrollToTopOfFeed();
     }
   }
@@ -37,28 +46,57 @@ class ReviewToolbar extends React.Component {
       <div id="toolbarContainer">
         <div id="toolbarSortText">Sort By</div>
 
-        <div id="dropdownHeader" onClick={(e) => this.handleSelection(e)}>
+        <div id="dropdownHeader" onClick={e => this.handleSelection(e)}>
           <span>{current}</span>
-          <span><img className="star" src={arrow} /></span>
+          <span>
+            <img className="star" src={arrow} />
+          </span>
         </div>
         <div className="dropdownContainer" style={{ display: display }}>
-          <div className="dropdownItem" value="Newest" onClick={(e) => {sortReviews(e.target.textContent.split(' ')[0]); this.handleSelection(e);}} >
+          <div
+            className="dropdownItem"
+            value=" Newest"
+            onClick={e => {
+              sortReviews(e.target.textContent.split(" ")[0]);
+              this.handleSelection(e);
+            }}
+          >
             <UnCheckedIcon />
             <span className="dropdownText">Newest</span>
           </div>
-          <div className="dropdownItem" value="Highest"onClick={(e) => {sortReviews(e.target.textContent.split(' ')[0]); this.handleSelection(e);}} >
+          <div
+            className="dropdownItem"
+            value="Highest"
+            onClick={e => {
+              sortReviews(e.target.textContent.split(" ")[0]);
+              this.handleSelection(e);
+            }}
+          >
             <UnCheckedIcon />
             <span className="dropdownText">Highest Rating</span>
           </div>
-          <div className="dropdownItem" value="Lowest" onClick={(e) => {sortReviews(e.target.textContent.split(' ')[0]); this.handleSelection(e);}} >
+          <div
+            className="dropdownItem"
+            value="Lowest"
+            onClick={e => {
+              sortReviews(e.target.textContent.split(" ")[0]);
+              this.handleSelection(e);
+            }}
+          >
             <UnCheckedIcon />
             <span className="dropdownText">Lowest Rating</span>
           </div>
         </div>
 
         <div id="filtersHeader">Filters</div>
-        <div>
-          {keyWords.map(keyWord => <FilterBox keyWord={keyWord} key={keyWord._id} filterReviews={filterReviews}/>)}
+        <div id="filterWords">
+          {keyWords.map(keyWord => (
+            <FilterBox
+              keyWord={keyWord}
+              key={keyWord._id}
+              filterReviews={filterReviews}
+            />
+          ))}
         </div>
       </div>
     );
@@ -70,5 +108,5 @@ ReviewToolbar.propTypes = {
   keyWords: PropTypes.array.isRequired,
   sortReviews: PropTypes.func.isRequired,
   filterReviews: PropTypes.func.isRequired,
-  scrollToTopOfFeed: PropTypes.func.isRequired,
+  scrollToTopOfFeed: PropTypes.func.isRequired
 };
