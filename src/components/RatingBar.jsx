6df1dfedf927a4805799a454d2879e78a163b8da
Jia <jiafeng3124@gmail.com>
2019-05-31 11:51:45 -0700

    formatting

--- src/components/RatingBar.jsx
+++ src/components/RatingBar.jsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styles from './RatingBar.css';
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./RatingBar.css";
 
 const RatingBar = ({ index, filter, scrollToTopOfFeed, percentages }) => {
   if (!percentages.length) {
     return <div>Loading...</div>;
   }
   return (
-      <div className="toolbarAndNumber" onClick={() => {filter(5 - index); scrollToTopOfFeed();}}>
-      <span className="toolbarNumber">{5 - index}</span>
+    <div
+      className="toolbarAndNumber"
+      onClick={() => {
+        filter(5 - index);
+        scrollToTopOfFeed();
+      }}
+    >
+      <div className="toolbarNumber">{5 - index}</div>
       <div className="toolbar-light-background">
-        <div className="toolbar-red" style={{ width: percentages[4 - index] }} key={percentages[4 - index]} />
+        <div
+          className="toolbar-red"
+          style={{ width: percentages[4 - index] }}
+          key={percentages[4 - index]}
+        />
       </div>
-      </div>
-  )
-}
+    </div>
+  );
+};
 
 export default RatingBar;
 
 RatingBar.propTypes = {
-    filter: PropTypes.func.isRequired,
-    scrollToTopOfFeed: PropTypes.func.isRequired,
-    percentages: PropTypes.array.isRequired,
-    index: PropTypes.number.isRequired,
+  filter: PropTypes.func.isRequired,
+  scrollToTopOfFeed: PropTypes.func.isRequired,
+  percentages: PropTypes.array.isRequired,
+  index: PropTypes.number.isRequired
 };
