02847e7486c666103e6ee37319e949cc9b09d4d4
Jia <jiafeng3124@gmail.com>
2019-05-31 11:51:11 -0700

    formatting

--- src/components/ReviewList.jsx
+++ src/components/ReviewList.jsx
@@ -1,15 +1,17 @@
-import React from 'react';
-import Review from './Review.jsx';
-import PropTypes from 'prop-types';
+import React from "react";
+import Review from "./Review.jsx";
+import PropTypes from "prop-types";
 
 const ReviewList = ({ reviews }) => (
   <div id="reviewsContainer">
-    {reviews.map(review => <Review review={review} key={review._id} />)}
+    {reviews.map(review => (
+      <Review review={review} key={review._id} />
+    ))}
   </div>
 );
 
 export default ReviewList;
 
-// ReviewList.propTypes = {
-//   reviews: PropTypes.array.isRequired,
-// };
+ReviewList.propTypes = {
+  reviews: PropTypes.array.isRequired
+};
