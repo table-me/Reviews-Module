83ee3bb6d0f2f5b76931c14dfce1c0463eacd91b
Jia <jiafeng3124@gmail.com>
2019-05-31 16:14:19 -0700

    add icon for sort list select

---
+++ src/components/CheckedIcon.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./CheckedIcon.css";
+
+const CheckedIcon = () => (
+  <svg height="24px" width="24px" viewBox="0 0 100 100">
+    <circle cx="50" cy="50" r="30" stroke-width="30" fill="white" />
+  </svg>
+);
+
+export default CheckedIcon;
