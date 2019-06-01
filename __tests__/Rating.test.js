7a99a2d25e50dc8d386cf67eac365baf7d7ac9c3
Jia <jiafeng3124@gmail.com>
2019-05-31 21:23:40 -0700

    Remove unneeded files

--- __tests__/Rating.test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import Rating from '../src/components/Rating.jsx';
-import App from '../src/components/App.jsx';
-import User from '../src/components/User.jsx';
-
-
