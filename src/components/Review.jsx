b5094203c307a265a158c10ff3c86dad2ad0375c
Jia <jiafeng3124@gmail.com>
2019-06-14 16:32:43 -0700

    delete unneeded module

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import PropTypes from "prop-types";
-import axios from "axios";
 import ReportPopUp from "./ReportPopUp.jsx";
 import moment from "moment";
 import styles from "./Review.css";
