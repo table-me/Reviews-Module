2c2ba33f57d71436f3374a7229c22c6edca13680
Jia <jiafeng3124@gmail.com>
2019-06-14 16:32:21 -0700

    fix css name

--- src/components/CheckedIcon.jsx
+++ src/components/CheckedIcon.jsx
@@ -3,7 +3,7 @@ import styles from "./CheckedIcon.css";
 
 const CheckedIcon = () => (
   <svg height="24px" width="24px" viewBox="0 0 100 100">
-    <circle cx="50" cy="50" r="30" stroke-width="30" fill="white" />
+    <circle cx="50" cy="50" r="30" strokeWidth="30" fill="white" />
   </svg>
 );
 
