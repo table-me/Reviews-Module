80f8cc73fe1ea9ffe865d263a1b377e163014f48
Jia <jiafeng3124@gmail.com>
2019-06-14 16:31:13 -0700

    delete module and added own id

--- database/Restaurant.js
+++ database/Restaurant.js
@@ -1,30 +1,33 @@
-const mongoose = require('mongoose');
-const AutoIncrement = require('mongoose-sequence')(mongoose);
-const db = require('./index.js');
+const mongoose = require("mongoose");
+// const AutoIncrement = require("mongoose-sequence")(mongoose);
+const db = require("./index.js");
 mongoose.Promise = global.Promise;
 
 const restaurantSchema = new mongoose.Schema({
+  id: Number,
   name: String,
   filters: [],
-  reviews: [{
-    username: String,
-    initials: String,
-    profilePic: String,
-    city: String,
-    review: String,
-    createdAt: Date,
-    noise: Number,
-    foodRating: Number,
-    ambianceRating: Number,
-    serviceRating: Number,
-    valueRating: Number,
-    overallRating: Number,
-    reviewCount: Number,
-    recommended: Number
-  }],
+  reviews: [
+    {
+      username: String,
+      initials: String,
+      profilePic: String,
+      city: String,
+      review: String,
+      createdAt: Date,
+      noise: Number,
+      foodRating: Number,
+      ambianceRating: Number,
+      serviceRating: Number,
+      valueRating: Number,
+      overallRating: Number,
+      reviewCount: Number,
+      recommended: Number
+    }
+  ]
 });
-restaurantSchema.plugin(AutoIncrement, { inc_field: 'id' });
+// restaurantSchema.plugin(AutoIncrement, { inc_field: "id" });
 
-const Restaurant = mongoose.model('Restaurant', restaurantSchema);
+const Restaurant = mongoose.model("Restaurant", restaurantSchema);
 
 module.exports = Restaurant;
