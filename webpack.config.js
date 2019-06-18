a0807f5db6b7f053efd2b89567909254060c772d
Jia <jiafeng3124@gmail.com>
2019-06-18 10:53:17 -0700

    add brotli and gzip

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,52 @@
-const path = require('path');
+const path = require("path");
+const BrotliGzipPlugin = require("brotli-gzip-webpack-plugin");
 
 module.exports = {
-  entry: path.join(__dirname, '/src/index.jsx'),
+  entry: path.join(__dirname, "/src/index.jsx"),
   output: {
-    filename: 'bundle.js',
-    path: path.join(__dirname, '/public/dist')
+    filename: "bundle.js",
+    path: path.join(__dirname, "/public/dist")
   },
+  mode: "production",
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ['babel-loader']
+        use: ["babel-loader"]
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: ["style-loader", "css-loader"]
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [{
-          loader: 'file-loader',
-          options: {
-            name: 'fonts/[name].[ext]'
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "fonts/[name].[ext]"
+            }
           }
-        }]
+        ]
       }
     ]
-  }
+  },
+  plugins: [
+    new BrotliGzipPlugin({
+      asset: "[path].br[query]",
+      algorithm: "brotli",
+      test: /\.(js|jsx|css|html|svg)$/,
+      threshold: 10240,
+      minRatio: 0.8,
+      quality: 11
+    }),
+    new BrotliGzipPlugin({
+      asset: "[path].gz[query]",
+      algorithm: "gzip",
+      test: /\.(js|jsx|css|html|svg)$/,
+      threshold: 10240,
+      minRatio: 0.8
+    })
+  ]
 };
-
-//   {
-//     test: /\.(png|svg|jpg|gif)$/,
-//     use: ['file-loader']
-//   },
