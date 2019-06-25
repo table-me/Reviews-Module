14d7c8eb6e3f1c78979358e3bf3eec044873d2b5
Jia <jiafeng3124@gmail.com>
2019-06-25 01:57:09 -0700

    Update index.js

--- database/index.js
+++ database/index.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://3.14.151.226/reviews", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost/reviews", { useNewUrlParser: true });
 const db = mongoose.connection;
 
 module.exports = db;
